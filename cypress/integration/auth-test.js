f64a4916d1739d4c521c36d5736ebf8c1608ad8b
chisha <aamaechi@hotmail.com>
2022-03-17 17:36:31 +0000

    Adding background styles and updating Cypress tests

--- cypress/integration/auth-test.js
+++ cypress/integration/auth-test.js
@@ -1,19 +1,16 @@
 describe("Displays Storybooks app login page", () => {
-    beforeEach(() => {
-      cy.visit("https://project-server-mongodb-oauth.herokuapp.com/");
-    });
-  
-    it("displays app titles", () => {
-      cy.contains("Login page");
-      cy.contains("Enjoy your read");
-      cy.contains("Read storybooks");
-    });
-  
-    it("displays Google login button", () => {
-      cy.contains("a", "Log in with Google").should("have.attr", "href");
-      cy.get(".btn");
-    });
-
-  
+  beforeEach(() => {
+    cy.visit("https://project-storybooks.herokuapp.com/");
   });
-  
+
+  it("displays app titles", () => {
+    cy.contains("Login page");
+    cy.contains("Enjoy reading storybooks here");
+    cy.contains("Creative stories");
+  });
+
+  it("displays Google login button", () => {
+    cy.contains("a", "Log in with Google").should("have.attr", "href");
+    cy.get(".btn");
+  });
+});
